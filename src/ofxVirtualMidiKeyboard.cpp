98010495a33e1e2a62bc846b64e3e09438e72b1c
Thomas Geissl <thomas.geissl@gmail.com>
2016-02-15 21:11:00 +0100

    splitted header and definition

---
+++ src/ofxVirtualMidiKeyboard.cpp
@@ -0,0 +1,125 @@
+#include "ofxVirtualMidiKeyboard.h"
+
+ofxVirtualMidiKeyboard::~ofxVirtualMidiKeyboard()
+{
+    ofRemoveListener(ofEvents().keyPressed, this, &ofxVirtualMidiKeyboard::keyPressed);
+    ofRemoveListener(ofEvents().keyReleased, this, &ofxVirtualMidiKeyboard::keyReleased);
+    _activeMappingParameter.removeListener(this, &ofxVirtualMidiKeyboard::activeMappingChanged);
+}
+
+void ofxVirtualMidiKeyboard::setup(string name, int channel, int velocity, int octaveShift)
+{
+    _midiOut.openVirtualPort(name);
+    ofAddListener(ofEvents().keyPressed, this, &ofxVirtualMidiKeyboard::keyPressed);
+    ofAddListener(ofEvents().keyReleased, this, &ofxVirtualMidiKeyboard::keyReleased);
+
+    _parameters.setName(name);
+    _activeParameter.set("On/off", true);
+    _channelParameter.set("Channel", channel, 1, 16);
+    _velocityParameter.set("Velocity", velocity, 0, 127);
+    _octaveShiftParameter.set("Octave shift", octaveShift, -6,6);
+
+    _parameters.add(_activeParameter);
+    _parameters.add(_channelParameter);
+    _parameters.add(_velocityParameter);
+    _parameters.add(_octaveShiftParameter);
+
+    _mappingParameters.setName("Mapping");
+    _activeMappingParameter.set("On/off", false);
+    _activeMappingParameter.addListener(this, &ofxVirtualMidiKeyboard::activeMappingChanged);
+    _mappingNoteParameter.set("Note", 0,0,11);
+    _mappingParameters.add(_activeMappingParameter);
+    _mappingParameters.add(_mappingNoteParameter);
+}
+
+void ofxVirtualMidiKeyboard::setActive(bool value)
+{
+    _activeParameter.set(value);
+}
+
+void ofxVirtualMidiKeyboard::setMappingActive(bool value)
+{
+    _activeMappingParameter.set(value);
+}
+
+void ofxVirtualMidiKeyboard::setMapping(int keys[])
+{
+    _keyMapping.clear();
+    for(int i = 0; i < 12; i++)
+    {
+        _keyMapping[keys[i], 60+i];
+    }
+
+}
+
+ofParameterGroup &ofxVirtualMidiKeyboard::getParameters()
+{
+    return _parameters;
+}
+
+ofParameterGroup &ofxVirtualMidiKeyboard::getMappingParameters()
+{
+    return _mappingParameters;
+}
+
+void ofxVirtualMidiKeyboard::keyPressed(ofKeyEventArgs &arg)
+{
+    if(!_activeParameter)
+    {
+        if(_activeMappingParameter)
+        {
+            assignKeyToCurrentNote(arg.key);
+        }
+        return;
+    }
+    if(_keyMapping.find(arg.key) != _keyMapping.end())
+    {
+        int note = _keyMapping[arg.key] + 12*_octaveShiftParameter;
+        _midiOut.sendNoteOn(_channelParameter, note);
+    }
+}
+
+void ofxVirtualMidiKeyboard::keyReleased(ofKeyEventArgs &arg)
+{
+    if(!_activeParameter)
+    {
+        return;
+    }
+    if(_keyMapping.find(arg.key) != _keyMapping.end())
+    {
+        int note = _keyMapping[arg.key] + 12*_octaveShiftParameter;
+        _midiOut.sendNoteOff(_channelParameter, note);
+    }
+}
+
+void ofxVirtualMidiKeyboard::activeMappingChanged(bool &value)
+{
+    if(value)
+    {
+        _activeParameter.set(!value);
+        _mappingNoteParameter = 0;
+    }
+    else
+    {
+        _mappingNoteParameter = 0;
+    }
+}
+
+void ofxVirtualMidiKeyboard::assignKeyToCurrentNote(int key)
+{
+    _keyMapping[key] = _mappingNoteParameter + 60;
+    if(_mappingNoteParameter < _mappingNoteParameter.getMax())
+    {
+        _mappingNoteParameter++;
+    }
+    else
+    {
+        _mappingNoteParameter = 0;
+        _activeMappingParameter = false;
+    }
+    if(_keyMapping.size() > 12)
+    {
+        _keyMapping.clear();
+        _mappingNoteParameter = 0;
+    }
+}
--- src/ofxVirtualMidiKeyboard.h
+++ src/ofxVirtualMidiKeyboard.h
@@ -5,124 +5,22 @@
 class ofxVirtualMidiKeyboard
 {
 public:
-    ~ofxVirtualMidiKeyboard()
-    {
-        ofRemoveListener(ofEvents().keyPressed, this, &ofxVirtualMidiKeyboard::keyPressed);
-        ofRemoveListener(ofEvents().keyReleased, this, &ofxVirtualMidiKeyboard::keyReleased);
-        _activeMappingParameter.removeListener(this, &ofxVirtualMidiKeyboard::activeMappingChanged);
-    }
+    ~ofxVirtualMidiKeyboard();
 
-    void setup(string name = "ofxMidiVirtualKeyboard", int channel = 1, int velocity = 127, int octaveShift = 0)
-    {
-        _midiOut.openVirtualPort(name);
-        ofAddListener(ofEvents().keyPressed, this, &ofxVirtualMidiKeyboard::keyPressed);
-        ofAddListener(ofEvents().keyReleased, this, &ofxVirtualMidiKeyboard::keyReleased);
+    void setup(string name = "ofxMidiVirtualKeyboard", int channel = 1, int velocity = 127, int octaveShift = 0);
+    void setActive(bool value = true);
+    void setMappingActive(bool value = true);
 
-        _parameters.setName(name);
-        _activeParameter.set("On/off", true);
-        _channelParameter.set("Channel", channel, 1, 16);
-        _velocityParameter.set("Velocity", velocity, 0, 127);
-        _octaveShiftParameter.set("Octave shift", octaveShift, -6,6);
-
-        _parameters.add(_activeParameter);
-        _parameters.add(_channelParameter);
-        _parameters.add(_velocityParameter);
-        _parameters.add(_octaveShiftParameter);
-
-        _mappingParameters.setName("Mapping");
-        _activeMappingParameter.set("On/off", false);
-        _activeMappingParameter.addListener(this, &ofxVirtualMidiKeyboard::activeMappingChanged);
-        _mappingNoteParameter.set("Note", 0,0,11);
-        _mappingParameters.add(_activeMappingParameter);
-        _mappingParameters.add(_mappingNoteParameter);
-    }
-    void setActive(bool value)
-    {
-        _activeParameter.set(value);
-    }
-    void setMappingActive(bool value)
-    {
-        _activeMappingParameter.set(value);
-    }
-
-    void setMapping(int keys[12])
-    {
-        _keyMapping.clear();
-        for(int i = 0; i < 12; i++)
-        {
-            _keyMapping[keys[i], 60+i];
-        }
-
-    }
-    ofParameterGroup & getParameters()
-    {
-        return _parameters;
-    }
-    ofParameterGroup & getMappingParameters()
-    {
-        return _mappingParameters;
-    }
+    void setMapping(int keys[12]);
+    ofParameterGroup & getParameters();
+    ofParameterGroup & getMappingParameters();
 
 protected:
 private:
-    void keyPressed(ofKeyEventArgs & arg)
-    {
-        if(!_activeParameter)
-        {
-            if(_activeMappingParameter)
-            {
-                assignKeyToCurrentNote(arg.key);
-            }
-            return;
-        }
-        if(_keyMapping.find(arg.key) != _keyMapping.end())
-        {
-            int note = _keyMapping[arg.key] + 12*_octaveShiftParameter;
-            _midiOut.sendNoteOn(_channelParameter, note);
-        }
-    }
-    void keyReleased(ofKeyEventArgs & arg)
-    {
-        if(!_activeParameter)
-        {
-            return;
-        }
-        if(_keyMapping.find(arg.key) != _keyMapping.end())
-        {
-            int note = _keyMapping[arg.key] + 12*_octaveShiftParameter;
-            _midiOut.sendNoteOff(_channelParameter, note);
-        }
-    }
-    void activeMappingChanged(bool & value)
-    {
-        if(value)
-        {
-            _activeParameter.set(!value);
-            _mappingNoteParameter = 0;
-        }
-        else
-        {
-            _mappingNoteParameter = 0;
-        }
-    }
-    void assignKeyToCurrentNote(int key)
-    {
-        _keyMapping[key] = _mappingNoteParameter + 60;
-        if(_mappingNoteParameter < _mappingNoteParameter.getMax())
-        {
-            _mappingNoteParameter++;
-        }
-        else
-        {
-            _mappingNoteParameter = 0;
-            _activeMappingParameter = false;
-        }
-        if(_keyMapping.size() > 12)
-        {
-            _keyMapping.clear();
-            _mappingNoteParameter = 0;
-        }
-    }
+    void keyPressed(ofKeyEventArgs & arg);
+    void keyReleased(ofKeyEventArgs & arg);
+    void activeMappingChanged(bool & value);
+    void assignKeyToCurrentNote(int key);
 
 private:
     ofxMidiOut _midiOut;
